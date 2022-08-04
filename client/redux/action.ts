4805aa606739604d4057dd9223f11608139dc2ce
lautarovollmer <lautarovollmer0@gmail.com>
2022-08-04 12:58:17 -0300

    actionsreducers

---
+++ client/redux/action.ts
@@ -0,0 +1,78 @@
+import axios from "axios";
+import {
+  GET_ALL_PRODUCTS,
+  GET_PRODUCTS_ID,
+  ADD_PRODUCTS,
+  CLEAN_PRODUCTS_DETAILS,
+} from "./types";
+
+export const getAllRecives = () => {
+  return async function (dispatch: any) {
+    try {
+      const products = await axios.get("http://localhost:3001/products");
+      console.log(products.data);
+      return dispatch({
+        type: GET_ALL_PRODUCTS,
+        payload: products.data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const addProducts = (props: any) => {
+  return async function (dispatch: any) {
+    try {
+      const r = await axios.post("http://localhost:3001/createproducts", props);
+      console.log(r.data);
+      dispatch({ type: ADD_PRODUCTS, payload: r.data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const getProductById = (id: any) => {
+  return async function (dispatch: any) {
+    try {
+      const r = await axios.get(`http://localhost:3001/products/${id}`);
+      dispatch({ type: GET_PRODUCTS_ID, payload: r.data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const cleanDetailsProducts = () => {
+  return async function () {
+    try {
+      console.log("MSG");
+      return { type: CLEAN_PRODUCTS_DETAILS };
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const deleteProducts = (id:any) => {
+  return async function (dispatch:any) {
+    try {
+      await axios.delete(`http://localhost:3001/products/${id}`);
+      dispatch(getAllRecives());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const editProducts = (id: any) => {
+  return async function (dispatch: any) {
+    try {
+      await axios.put(`http://localhost:3001/products/${id}`);
+      dispatch(getAllRecives());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
---
+++ client/redux/reducer.ts
@@ -0,0 +1,34 @@
+import { combineReducers } from "redux";
+import {
+  GET_ALL_PRODUCTS,
+  GET_PRODUCTS_ID,
+  CLEAN_PRODUCTS_DETAILS,
+} from "./types";
+
+const initialData = {
+  allProducts: [],
+  allBrands: [],
+  assistantProducts: [],
+  productsDetail: [],
+};
+
+ function rootReducer(state = initialData, action: any) {
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return {
+        ...state,
+        allProducts: action.payload,
+        assistantProducts: action.payload,
+      };
+    case GET_PRODUCTS_ID:
+      return { ...state, pokemon: action.payload };
+    
+    case CLEAN_PRODUCTS_DETAILS :
+        return{...state, allProducts: [],}    
+  
+    default:
+      return  state ;
+  }
+}
+
+export default combineReducers({rootReducer});
